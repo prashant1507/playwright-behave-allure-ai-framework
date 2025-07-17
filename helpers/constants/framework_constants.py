dc7c51513883bfeb30e28ab1d37394cf7dbe7240
mypc <pjeet.0733@gmail.com>
2025-07-17 14:35:20 +0400

    framework_constants.py

--- helpers/constants/framework_constants.py
+++ helpers/constants/framework_constants.py
@@ -1,3 +1,9 @@
 import os.path
 
 CONFIG_YAML = os.path.join(os.getcwd(), 'config.yaml')
+
+REPORTS = os.path.join(os.getcwd(), 'reports')
+
+# Tracing
+TRACES_DIR = os.path.join(REPORTS, "traces")
+TRACES_VIDEOS_DIR = os.path.join(TRACES_DIR, "videos")
